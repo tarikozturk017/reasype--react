9db4c1417a0e576afac51948b6430a36b6576879
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-11-10 21:04:57 -0500

    toggle advance functionality added

--- reasype/src/components/Filter.js
+++ reasype/src/components/Filter.js
@@ -17,6 +17,8 @@ export default function Filter(props) {
         time: ''
     }) 
 
+    const [isAdvanced, setIsAdvanced] = React.useState(false)
+
     const mealTypes = ['Breakfast', 'Lunch', 'Dinner', 'Snack', 'Teatime']
     const dishTypes = ['Alcohol-cocktail', 'Biscuits and cookies', 'Bread', 'Cereals', 
         'Condiments and sauces', 'Drinks', 'Desserts', 'Egg', 'Main course', 'Omelet', 
@@ -85,6 +87,68 @@ export default function Filter(props) {
         )
     })
 
+    function toggleAdvancedSearch() {
+        console.log('setting adv/reg')
+        setIsAdvanced((oldAdvanced) => !oldAdvanced)
+    }
+
+    const regularSearch = <div className="advanced-search--button">
+                                <button onClick={toggleAdvancedSearch}>Advanced search</button>
+                            </div>
+
+    const advancedSearch = 
+    <div className="advanced--outer">
+    <div className="advanced-filter--container">
+        <div className="advanced-element--container" id="advanced-1">
+            <p className="input--description">Enter calorie range</p>
+            <RangeInput name="calories" handleRange={handleRange} />   
+        </div>       
+        <div className="advanced-element--container" id="advanced-2">
+            <p className="input--description">Enter preparation time</p>
+            <RangeInput name="time" handleRange={handleRange} />
+        </div>
+        
+        <div className="advanced-element--container" id="advanced-3">
+            <p className="input--description">Select dish type</p>
+            <EnumInput value={recipeData.dishType} name="dishType" placeholder="Dish type" handleEnumChange={handleEnumChange} options={dishTypes}/>
+        </div>
+        <div className="advanced-element--container" id="advanced-4">
+            <p className="input--description">Select meal type</p>
+            <EnumInput value={recipeData.mealType} name="mealType" placeholder="Meal type" handleEnumChange={handleEnumChange} options={mealTypes}/>
+        </div>
+        <div className="advanced-element--container" id="advanced-5">
+            <p className="input--description">Select cuisine type</p>
+            <EnumInput value={recipeData.cuisineType} name="cuisineType" placeholder="Cuisine type" handleEnumChange={handleEnumChange} options={cuisineTypes}/>
+        </div>
+        <div className="advanced-element--container" id="advanced-6">
+            <p className="input--description">Select health label</p>
+            <EnumInput value={recipeData.health} name="health" placeholder="Health label" handleEnumChange={handleEnumChange} options={healths}/>
+        </div>
+        <div className="advanced-element--container" id="advanced-7">
+            <p className="input-exclude--description">Enter an ingredient to exclude</p>
+            <input 
+                type="text" // string
+                placeholder="Exclude ingredient"
+                className="form--input"
+                value={recipeData.excluded}
+                onChange={handleChange}
+                name="excluded"
+                autoComplete="off"
+            />
+        </div>
+        <div></div>
+        <div className="advanced-element--container" id="advanced-paper">
+            <div><img src={OldPaper} alt="" /></div>
+        </div>
+    
+
+    </div>
+    <div  className="advanced-element--container" id="advanced-8">
+        <button onClick={toggleAdvancedSearch}>Return to basic search</button>
+    </div>
+    </div>
+
+    const updatedPage = isAdvanced ? advancedSearch : regularSearch;
 
     // TODO
     //handle data options with arr for enums -> [] when setting. push etc.. to manipulate  
@@ -123,6 +187,7 @@ export default function Filter(props) {
                     </div>
                 </div>
 
+                {/* <div className="advanced--outer">
                 <div className="advanced-filter--container">
                     <div className="advanced-element--container" id="advanced-1">
                         <p className="input--description">Enter calorie range</p>
@@ -169,8 +234,17 @@ export default function Filter(props) {
 
                 </div>
                 <div  className="advanced-element--container" id="advanced-8">
-                    <button >Return to basic search</button>
+                    <button onClick={toggleAdvancedSearch}>Return to basic search</button>
                 </div>
+                </div> */}
+                {/* <div className="advanced-search--button">
+                    <button onClick={toggleAdvancedSearch}>Advanced search</button>
+                </div> */}
+                
+                {/* {advancedSearch}
+                {regularSearch} */}
+
+                {updatedPage}
                 <div className="generate-button">
                     <button 
                         // className="generate-button"
