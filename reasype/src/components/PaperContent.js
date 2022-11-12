4118921f4fc4e49c445d21f23b631a3798153bcc
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-11-11 19:49:31 -0500

    paper desing updated

--- reasype/src/components/PaperContent.js
+++ reasype/src/components/PaperContent.js
@@ -4,6 +4,8 @@ export default function PaperContent(props) {
     // console.log(props)
     const data = props.data;
 
+    const OptionVal = data.ingr=== 100 ? '10+' : data.ingr;
+
     function enumElements(enumData) {
         let str = "";
         enumData.map((data, i, enumData) => {
@@ -18,16 +20,16 @@ export default function PaperContent(props) {
         <div className="paper-p--container">
             {/* {data.q && <p>Querying for: {data.q}</p>}
             {data.calories && <p>Calorie: {data.calories} <p>Note: total calorie of whole meal</p> </p>} */}
-            <p>Querying for: {data.q}</p>
-            <p>Max of <strong>{data.ingr}</strong> ingredients</p>
-            <p>Diet type(s): {enumElements(data.diet)}</p>
-            <p>Calories: {data.calories} kcal <i>(whole meal)</i></p>
-            <p>Preparation time: {data.time} mins</p>
-            <p>Dish type(s): {enumElements(data.dishType)}</p>
-            <p>Meal type(s): {enumElements(data.mealType)}</p>
-            <p>Cuisine type(s): {enumElements(data.cuisineType)}</p>
-            <p>Health label(s): {enumElements(data.health)}</p>
-            <p>Excluded: {data.excluded}</p>
+            {data.q && <li>Querying for: {data.q}</li>}
+            {OptionVal && <li>Max <strong>{OptionVal}</strong> ingredients</li>}
+            {data.diet.length!==0 && <li>Diet type(s): {enumElements(data.diet)}</li>}
+            {data.calories && <li>Calories: {data.calories} kcal <i>(whole meal)</i></li>}
+            {data.time && <li>Preparation time: {data.time} mins</li>}
+            {data.dishType.length!==0 && <li>Dish type(s): {enumElements(data.dishType)}</li>}
+            {data.mealType.length!==0 && <li>Meal type(s): {enumElements(data.mealType)}</li>}
+            {data.cuisineType.length!==0 && <li>Cuisine type(s): {enumElements(data.cuisineType)}</li>}
+            {data.health.length!==0 && <li>Health label(s): {enumElements(data.health)}</li>}
+            {data.excluded && <li>Excluded: <span className="excluded">{data.excluded}</span></li>}
         </div>
     )
 }
