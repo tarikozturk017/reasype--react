634f39217c948926e2f510ca40f5760f20778eef
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-11-16 21:36:32 -0500

    Update Filter.js

--- reasype/src/components/Filter.js
+++ reasype/src/components/Filter.js
@@ -2,7 +2,6 @@ import React from "react";
 import EnumInput from "./EnumInput";
 import RangeInput from "./RangeInput"
 import OldPaper from "../old.webp"
-// import Post from "../post.png"
 import PaperContent from "./PaperContent";
 import BasicPaper from "./BasicPaper";
 
@@ -38,27 +37,14 @@ export default function Filter(props) {
     const numbers = ['3', '4', '5', '6', '7', '8', '9', '10+']
 
 
-    // React.useEffect(() => {
-
-    // })
-
     function getRecipe() {
-        // const randomNumber = Math.floor(Math.random() * 10)
-        // const url = allMemeImages[randomNumber].url
-        // setMeme(prevMeme => ({
-        //     ...prevMeme,
-        //     randomImage: url
-        // }))
-        props.handleFilter(recipeData);
-        
+        props.handleFilter(recipeData);   
     }
-
 
     function handleRange(data, name) {
         console.log('setting handle range data: ' + data+name)
         setRecipeData(prevData => ({
             ...prevData,
-            // [name]: value
             [name]: data
         }))
     }
@@ -66,7 +52,6 @@ export default function Filter(props) {
     function resetRange(name){
         setRecipeData(prevData => ({
             ...prevData,
-            // [name]: value
             [name]: ''
         }))
     }
@@ -78,16 +63,13 @@ export default function Filter(props) {
         }
         setRecipeData(prevData => ({
             ...prevData,
-            // [name]: value
             [name]: value
         }))
     }
 
     function handleEnumChange(data, name) {
-        // console.log('Filter handleEnumChange' + data + name)
         setRecipeData(prevData => ({
             ...prevData,
-            // [name]: value
             [name]: data
         }))
     }
@@ -99,13 +81,10 @@ export default function Filter(props) {
     })
 
     function toggleAdvancedSearch() {
-        // console.log('setting adv/reg')
         setIsAdvanced((oldAdvanced) => !oldAdvanced)
         
     }
     
-    
-
     const regularSearch = <div className="advanced-search--button">
                                 <button onClick={toggleAdvancedSearch}>Advanced search</button>
                             </div>
@@ -150,7 +129,6 @@ export default function Filter(props) {
                 autoComplete="off"
             />
         </div>
-        {/* <div></div> */}
         <div className="advanced-element--container" id="advanced-paper">
             <div><img src={OldPaper} alt="" /></div>
             <PaperContent data={recipeData}/>
@@ -166,14 +144,6 @@ export default function Filter(props) {
     const updatedPage = isAdvanced ? advancedSearch : regularSearch;
     const OptionVal = recipeData.ingr=== 100 ? '10+' : recipeData.ingr;
 
-    // TODO
-    // add reset to range or make it toggle
-    // if no input press add, make no change
-    // sticker for basic search
-    // test project 
-
-    // apply the concept below to the enum types
-    // https://www.w3schools.com/howto/howto_js_filter_dropdown.asp
     return (
         <div className="filter-page--container">
             <div className="filter--header">
@@ -197,9 +167,7 @@ export default function Filter(props) {
                     <div className="form-element--container">
                         <p className="input--description">Select diet type</p>
                         <EnumInput value={recipeData.diet} name="diet" placeholder="Diet" handleEnumChange={handleEnumChange} options={diets}/>
-                        {/* <p className="diet-value--filter">{recipeData.diet.map((item) => {
-                            return <li>{item}</li>
-                        })}</p> */}
+                        
                     </div>
                     <div className="form-element--container">
                         <p>Max ingredients #{recipeData.ingr === 100 ? '10+' : parseInt(recipeData.ingr) }</p>
@@ -209,94 +177,16 @@ export default function Filter(props) {
                     </div>
                 </div>
 
-                {/* <div className="advanced--outer">
-                <div className="advanced-filter--container">
-                    <div className="advanced-element--container" id="advanced-1">
-                        <p className="input--description">Enter calorie range</p>
-                        <RangeInput name="calories" handleRange={handleRange} />   
-                    </div>       
-                    <div className="advanced-element--container" id="advanced-2">
-                        <p className="input--description">Enter preparation time</p>
-                        <RangeInput name="time" handleRange={handleRange} />
-                    </div>
-                    
-                    <div className="advanced-element--container" id="advanced-3">
-                        <p className="input--description">Select dish type</p>
-                        <EnumInput value={recipeData.dishType} name="dishType" placeholder="Dish type" handleEnumChange={handleEnumChange} options={dishTypes}/>
-                    </div>
-                    <div className="advanced-element--container" id="advanced-4">
-                        <p className="input--description">Select meal type</p>
-                        <EnumInput value={recipeData.mealType} name="mealType" placeholder="Meal type" handleEnumChange={handleEnumChange} options={mealTypes}/>
-                    </div>
-                    <div className="advanced-element--container" id="advanced-5">
-                        <p className="input--description">Select cuisine type</p>
-                        <EnumInput value={recipeData.cuisineType} name="cuisineType" placeholder="Cuisine type" handleEnumChange={handleEnumChange} options={cuisineTypes}/>
-                    </div>
-                    <div className="advanced-element--container" id="advanced-6">
-                        <p className="input--description">Select health label</p>
-                        <EnumInput value={recipeData.health} name="health" placeholder="Health label" handleEnumChange={handleEnumChange} options={healths}/>
-                    </div>
-                    <div className="advanced-element--container" id="advanced-7">
-                        <p className="input-exclude--description">Enter an ingredient to exclude</p>
-                        <input 
-                            type="text" // string
-                            placeholder="Exclude ingredient"
-                            className="form--input"
-                            value={recipeData.excluded}
-                            onChange={handleChange}
-                            name="excluded"
-                            autoComplete="off"
-                        />
-                    </div>
-                    <div></div>
-                    <div className="advanced-element--container" id="advanced-paper">
-                        <div><img src={OldPaper} alt="" /></div>
-                    </div>
-                
-
-                </div>
-                <div  className="advanced-element--container" id="advanced-8">
-                    <button onClick={toggleAdvancedSearch}>Return to basic search</button>
-                </div>
-                </div> */}
-                {/* <div className="advanced-search--button">
-                    <button onClick={toggleAdvancedSearch}>Advanced search</button>
-                </div> */}
-                
-                {/* {advancedSearch}
-                {regularSearch} */}
-
                 {updatedPage}
                 <BasicPaper data={recipeData} isAdvanced={isAdvanced}/> 
                 <div className="generate-button">
                     <button 
-                        // className="generate-button"
                         onClick={getRecipe}
                     >
                         Generate Recipe
                     </button>
                 </div>
-                {/* {!isAdvanced && {Post}} */}
-                {/* <div className="basic-paper--container" >
-                    {!isAdvanced && <div className="basic--paper">
-                        <img src={Post} alt="" />
-                    </div>}
-                    <div className="basic-p--paper">
-                        <li>Querying for Chicken</li>
-                        <li>Max 5 ingredients</li>
-                        <li>Diet types: </li>
-                        </div>
-                </div> */}
-                
-                
-                
             </div>
-            {/* <div className="meme">
-            <img src={meme.randomImage} alt="" className="meme--image" />
-                <h2 className="meme--text top">{meme.topText}</h2>
-                <h2 className="meme--text bottom">{meme.bottomText}</h2>
-            </div> */}
-            
         </div>
     )
 }
